91b3a23ad0ff197b1169255227401b3b9722d83d
Zack Cerza <zcerza@redhat.com>
2005-10-10 19:17:59 +0000

    Port to procedural API.
    * examples/filechooser-stress-test.py: Port to procedural API.

--- examples/filechooser-stress-test.py
+++ examples/filechooser-stress-test.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # Stress test: repeatedly open and close the filechooser dialog
 
-import dogtail.tree
-from dogtail.apps.wrappers.gcalctool import *
-import dogtail.utils
+from dogtail.procedural import *
 
-dogtail.utils.run('gedit')
-app = dogtail.tree.root.application('gedit')
+run('gedit')
 
 while True:
-	app.menu('File').menuItem('Open...').click()
-	app.dialog('Open File...').button('Cancel').click()
+	click('Open...')
+	focus.dialog('Open File...')
+	click('Cancel')
 

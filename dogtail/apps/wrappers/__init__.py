0b3f503893fdcefd659d95d3553a61262ae1b7c3
Zack Cerza <zcerza@redhat.com>
2006-01-13 19:44:47 +0000

    Bump version numbers.
    * setup.py, dogtail/__init__.py, dogtail/apps/__init__.py,
    dogtail/apps/wrappers/__init__.py: Bump version numbers.

--- dogtail/apps/wrappers/__init__.py
+++ dogtail/apps/wrappers/__init__.py
@@ -6,7 +6,7 @@ Author: David Malcolm <dmalcolm@redhat.com>
 """
 
 __author__ = "David Malcolm <dmalcolm@redhat.com>"
-__version__ = "0.4.4"
+__version__ = "0.5.0"
 __copyright__ = "Copyright © 2005 Red Hat, Inc."
 __license__ = "GPL"
 

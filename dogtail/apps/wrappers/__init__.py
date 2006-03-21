3659dc0b42c4bb62cc63f69e3cd1f8c07c9a973b
Zack Cerza <zcerza@redhat.com>
2006-03-21 20:19:14 +0000

    Add missing note about release in ChangeLog.
    * dogtail/__init__.py, dogtail/apps/__init__.py,
    dogtail/apps/wrappers/__init__.py, setup.py, NEWS: Post-release version bump.

--- dogtail/apps/wrappers/__init__.py
+++ dogtail/apps/wrappers/__init__.py
@@ -6,7 +6,7 @@ Author: David Malcolm <dmalcolm@redhat.com>
 """
 
 __author__ = "David Malcolm <dmalcolm@redhat.com>"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __copyright__ = "Copyright © 2005 Red Hat, Inc."
 __license__ = "GPL"
 

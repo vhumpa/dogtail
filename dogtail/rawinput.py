c9f72c79de5fc5e266b05f9f3d8a7b9b0e8b12e5
Zack Cerza <zcerza@redhat.com>
2006-07-24 21:20:39 +0000

    Use gtk.kesyms to look up keysyms. Retain compatibility with old names via
    * dogtail/rawinput.py: Use gtk.kesyms to look up keysyms. Retain
    compatibility with old names via a keySymAliases dict. Also, use only one
    atspi.EventGenerator.

--- dogtail/rawinput.py
+++ dogtail/rawinput.py
@@ -13,13 +13,13 @@ Zack Cerza <zcerza@redhat.com>
 import atspi
 from utils import doDelay
 from logging import debugLogger as logger
+ev = atspi.EventGenerator()
 
 def click (x, y, button = 1):
     """
     Synthesize a mouse button click at (x,y)
     """
     logger.log("Mouse button %s click at (%s,%s)"%(button,x,y))
-    ev = atspi.EventGenerator()
     ev.click(x, y, button)
     doDelay()
 
@@ -28,7 +28,6 @@ def doubleClick (x, y, button = 1):
     Synthesize a mouse button double-click at (x,y)
     """
     logger.log("Mouse button %s doubleclick at (%s,%s)"%(button,x,y))
-    ev = atspi.EventGenerator()
     ev.doubleClick(x,y,button)
     doDelay()
 
@@ -37,7 +36,6 @@ def press (x, y, button = 1):
     Synthesize a mouse button press at (x,y)
     """
     logger.log("Mouse button %s press at (%s,%s)"%(button,x,y))
-    ev = atspi.EventGenerator()
     ev.press(x,y, button)
     doDelay()
 
@@ -46,7 +44,6 @@ def release (x, y, button = 1):
     Synthesize a mouse button release at (x,y)
     """
     logger.log("Mouse button %s release at (%s,%s)"%(button,x,y))
-    ev = atspi.EventGenerator()
     ev.release(x,y,button)
     doDelay()
 
@@ -55,13 +52,11 @@ def absoluteMotion (x, y):
     Synthesize mouse absolute motion to (x,y)
     """
     logger.log("Mouse absolute motion to (%s,%s)"%(x,y))
-    ev = atspi.EventGenerator()
     ev.absoluteMotion(x,y)
     doDelay()
 
 def relativeMotion (x, y):
     logger.log("Mouse relative motion of (%s,%s)"%(x,y))
-    ev = atspi.EventGenerator()
     ev.relativeMotion(x,y)
     doDelay()
 
@@ -71,7 +66,6 @@ def drag(fromXY, toXY, button = 1):
     Wraps atspi.EventGenerator.drag, but with logging and delays.
     """
     logger.log("Mouse button %s drag from %s to %s"%(button, fromXY, toXY))
-    ev = atspi.EventGenerator()
 
     # A direct call to ev.drag doesn't work, delays seem to be needed
     # for nautilus icon view to work, at least:
@@ -91,13 +85,14 @@ def typeText(string):
     """
     Types the specified string, one character at a time.
     """
-    ev = atspi.EventGenerator()
     ev.injectKeyboardString(string)
 
-keySyms = {
-        'enter' : 0xff0d,
-        'esc' : 0xff1b,
-        'space' : 0x20
+import gtk.keysyms
+keySyms = gtk.keysyms.__dict__
+
+keySymAliases = {
+        'enter' : 'Return',
+        'esc' : 'Escape'
 }
 
 def pressKey(keyName):
@@ -106,6 +101,7 @@ def pressKey(keyName):
     keyName is the English name of the key as seen on the keyboard. Ex: 'enter'
     Names are looked up in the keySyms dict.
     """
-    ev = atspi.EventGenerator()
-    keySym = keySyms[keyName.lower()]
+    keyName = keySymAliases.get(keyName.lower(), keyName)
+    keySym = keySyms[keyName]
     ev.generateKeyboardEvent(keySym, "", atspi.SPI_KEY_SYM)
+

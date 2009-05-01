5d3cdf14f0216cd17481b28ce52852f8c08dc73f
Zack Cerza <zcerza@redhat.com>
2009-05-01 13:28:39 -0400

    Refactor most of dogtail.sessions.

--- dogtail/sessions.py
+++ dogtail/sessions.py
@@ -15,18 +15,42 @@ def scratchFile(label):
     return tempfile.NamedTemporaryFile(prefix = "%s%s." % (prefix, label), 
             dir = config.scratchDir, delete = False)
 
-class XServer(object):
-    def __init__(self, binary = "/usr/bin/Xorg", 
+class Subprocess(object):
+    def __init__(self, cmdList, environ = None):
+        self.cmdList = cmdList
+        self.environ = environ
+        self._exitCode = None
+
+    def start(self):
+        if self.environ == None: environ = os.environ
+        self.popen = subprocess.Popen(self.cmdList,
+                env = self.environ)#, stdout = subprocess.PIPE, 
+                #stderr = subprocess.STDOUT, close_fds = True)
+        return self.popen.pid
+
+    def wait(self):
+        return self.popen.wait()
+
+    def stop(self):
+        self.popen.terminate()
+
+    @property
+    def exitCode(self):
+        if self._exitCode == None:
+            self._exitCode = self.wait()
+        return self._exitCode
+
+class XServer(Subprocess):
+    def __init__(self, server = "/usr/bin/Xorg", 
             xinitrc = "/etc/X11/xinit/Xclients", 
             resolution = "1024x768x16"):
         """resolution is only used with Xvfb."""
+        self.server = server
+        self._exitCode = None
         self.xinit = "/usr/bin/xinit"
         self.display = None
-        self.binary = binary
         self.xinitrc = xinitrc
         self.resolution = resolution
-        self.exitCode = None
-        self.running = False
 
     @staticmethod
     def findFreeDisplay():
@@ -40,75 +64,43 @@ class XServer(object):
         usedDisplays.sort()
         return ':' + str(usedDisplays[-1] + 1)
 
-    def _buildCommand(self):
+    @property
+    def cmdList(self):
         self.display = self.findFreeDisplay()
         cmd = []
         if self.xinit:
             cmd.append(self.xinit)
             if self.xinitrc: cmd.append(self.xinitrc)
             cmd.append('--')
-        cmd.append(self.binary)
+        cmd.append(self.server)
         cmd.append(self.display)
         cmd.extend(['-ac', '-noreset'])
-        if self.binary.endswith('Xvfb'):
+        if self.server.endswith('Xvfb'):
             cmd.extend(['-screen', '0', self.resolution])
             cmd.append('-shmem')
         return cmd
 
     def start(self):
-        cmd = self._buildCommand()
-        print ' '.join(cmd)
-        self.popen = subprocess.Popen(cmd)
-        self.running = True
+        print ' '.join(self.cmdList)
+        self.popen = subprocess.Popen(self.cmdList)
         return self.popen.pid
 
-    def wait(self):
-        self.exitCode = self.popen.wait()
-        self.running = False
-        return self.exitCode
-
-    def stop(self):
-        self.popen.terminate()
-        self.running = False
-
-class Script(object):
-    def __init__(self, name, args = []):
-        self.name = name
-        self.args = args
-        self.popen = None
-        self.exitCode = None
-
-    def start(self, environ = None):
-        if environ == None: environ = os.environ
-        self.popen = subprocess.Popen(self.args, executable = self.name,
-                env = environ)#, stdout = subprocess.PIPE, 
-                #stderr = subprocess.STDOUT, close_fds = True)
-        self.running = True
-        return self.popen.pid
-
-    def wait(self):
-        self.exitCode = self.popen.wait()
-        self.running = False
-        return self.exitCode
-
-    def stop(self):
-        self.popen.terminate()
-        self.running = False
-
+class Script(Subprocess):
+    pass
 
 class Session(object):
 
     cookieName = "DOGTAIL_SESSION_COOKIE"
 
-    def __init__(self, sessionBinary, scriptName, scriptArgs = [], scriptDelay = 10, logout = True):
+    def __init__(self, sessionBinary, scriptCmdList, scriptDelay = 10, logout = True):
         self.sessionBinary = sessionBinary
-        self.script = Script(scriptName, scriptArgs)
+        print scriptCmdList
+        self.script = Script(scriptCmdList)
         self.scriptDelay = scriptDelay
         self.logout = logout
         self.xserver = XServer()
-        self.__cookie = None
-        self.__environment = None
-        self.exitCodeFile = scratchFile("exitcode").name
+        self._cookie = None
+        self._environment = None
 
     def start(self):
         xinitrcFileObj = scratchFile('xinitrc')
@@ -116,13 +108,13 @@ class Session(object):
         self._buildXInitRC(xinitrcFileObj)
         xServerPid = self.xserver.start()
         time.sleep(self.scriptDelay)
-        scriptEnv = self.environment
-        scriptPid = self.script.start(scriptEnv)
+        self.script.environ = self.environment
+        scriptPid = self.script.start()
         return (xServerPid, scriptPid)
 
     @property
     def environment(self):
-        if self.__environment: return self.__environment
+        if self._environment: return self._environment
         def getEnvDict(fileName):
             try: envString = open(fileName, 'r').read()
             except IOError: return {}
@@ -144,41 +136,31 @@ class Session(object):
             pid = envFile.split('/')[2]
             if pid == 'self' or pid == str(os.getpid()): continue
             envDict = getEnvDict(envFile)
-            if isSessionEnv(envDict): self.__environment = envDict
-        if not self.__environment:
+            if isSessionEnv(envDict): self._environment = envDict
+        if not self._environment:
             raise RuntimeError("Can't find our environment!")
-        return self.__environment
+        return self._environment
 
     def wait(self):
         self.script.wait()
         return self.xserver.wait()
 
     def stop(self):
-        self.script.stop()
+        try: self.script.stop()
+        except OSError: pass
         self.xserver.stop()
 
     def attemptLogout(self):
-        logoutScript = Script('/tmp/dt_git/scripts/dogtail-logout')
-        logoutScript.start(environ = self.environment)
+        logoutScript = Script('/tmp/dt_git/scripts/dogtail-logout', 
+                environ = self.environment)
+        logoutScript.start()
         logoutScript.wait()
 
     @property
     def cookie(self):
-        if not self.__cookie:
-            self.__cookie = "%X" % random.getrandbits(16)
-        return self.__cookie
-
-    @property
-    def running(self):
-        return self.xserver.running
-
-    @property
-    def scriptExitCode(self):
-        if self.running: self.wait()
-        try:
-            return int(file(self.exitCodeFile, 'r').read())
-        except ValueError:
-            return None
+        if not self._cookie:
+            self._cookie = "%X" % random.getrandbits(16)
+        return self._cookie
 
     def _buildXInitRC(self, fileObj):
         if self.logout:
@@ -190,7 +172,6 @@ class Session(object):
             "gconftool-2 --type bool --set /desktop/gnome/interface/accessibility true",
             ". /etc/X11/xinit/xinitrc-common",
             "export %s" % self.cookieName,
-            #"sh -c \"sleep 10; cd %s && dogtail-detect-session && sh -c \\\"%s; echo -n \\\\\\$? > %s\\\"%s\"&\n" % (os.getcwdu(), self.script, self.exitCodeFile, logoutString),
             "exec -l $SHELL -c \"$CK_XINIT_SESSION $SSH_AGENT %s\"" % \
                     self.sessionBinary,
             ""]
@@ -198,3 +179,4 @@ class Session(object):
         fileObj.write('\n'.join(lines).strip())
         fileObj.flush()
         fileObj.close()
+

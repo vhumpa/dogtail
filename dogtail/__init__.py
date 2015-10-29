8aeeec494fabf2fdf47f9951832d4794852248f2
Vitezslav Humpa <vhumpa@redhat.com>
2015-10-29 10:34:03 +0100

    0.9.1 version bump-up

--- dogtail/__init__.py
+++ dogtail/__init__.py
@@ -9,7 +9,7 @@ __author__ = """Zack Cerza <zcerza@redhat.com>,
 Ed Rousseau <rousseau@redhat.com>,
 David Malcolm <dmalcolm@redhat.com>,
 Vita Humpa <vhumpa@redhat.com>"""
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __copyright__ = "Copyright © 2005-2014 Red Hat, Inc."
 __license__ = "GPL"
 __all__ = ("config", "predicate",
--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def session_file():
 
 setup (
         name = 'dogtail',
-        version = '0.9.0',
+        version = '0.9.1',
         description = """GUI test tool and automation framework that uses Accessibility (a11y) technologies to communicate with desktop applications.""",
         author = """Zack Cerza <zcerza@redhat.com>,
 Ed Rousseau <rousseau@redhat.com>,

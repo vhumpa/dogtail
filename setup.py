d9482a9c8758c909160e3040585b931c5e73d912
Matěj Cepl <mcepl@cepl.eu>
2015-11-02 20:28:40 +0100

    PEP8ization of setup.py

--- setup.py
+++ setup.py
@@ -6,26 +6,26 @@ from distutils.command.bdist_rpm import bdist_rpm
 
 def examples():
     import os
-    exList = os.listdir(os.curdir + '/examples/')
+    ex_list = os.listdir(os.curdir + '/examples/')
     result = []
-    for ex in exList:
+    for ex in ex_list:
         if ex.split('.')[-1] == 'py':
             result = result + ['examples/' + ex]
     return result
 
 def examples_data():
     import os
-    dataList = os.listdir(os.curdir + '/examples/data/')
+    data_list = os.listdir(os.curdir + '/examples/data/')
     result = []
-    for data in dataList:
-		result = result + ['examples/data/' + data]
+    for data in data_list:
+        result = result + ['examples/data/' + data]
     return result
 
 def tests():
     import os
-    exList = os.listdir(os.curdir + '/tests/')
+    ex_list = os.listdir(os.curdir + '/tests/')
     result = []
-    for ex in exList:
+    for ex in ex_list:
         if ex.split('.')[-1] == 'py':
             result = result + ['tests/' + ex]
     return result
@@ -53,41 +53,43 @@ def scripts():
     list = os.listdir(os.curdir + '/scripts/')
     result = ['sniff/sniff']
     for file in list:
-		result = result + ['scripts/' + file]
+        result = result + ['scripts/' + file]
     return result
 
 def session_file():
     result = ['scripts/gnome-dogtail-headless.session']
     return result
 
-setup (
-        name = 'dogtail',
-        version = '0.9.1',
-        description = """GUI test tool and automation framework that uses Accessibility (a11y) technologies to communicate with desktop applications.""",
-        author = """Zack Cerza <zcerza@redhat.com>,
+setup(
+    name='dogtail',
+    version='0.9.1',
+    description="GUI test tool and automation framework " +
+    "that uses Accessibility (a11y) technologies to " +
+    "communicate with desktop applications.",
+    author="""Zack Cerza <zcerza@redhat.com>,
 Ed Rousseau <rousseau@redhat.com>,
 David Malcolm <dmalcolm@redhat.com>
 Vitezslav Humpa <vhumpa@redhat.com>""",
-        author_email = 'dogtail-list@gnome.org',
-        url = 'http://dogtail.fedorahosted.org/',
-        packages = ['dogtail'],
-        scripts = scripts(),
-        data_files = [
-                                ('share/doc/dogtail/examples',
-                                        examples() ),
-                                ('share/doc/dogtail/examples/data',
-                                        examples_data() ),
-                                ('share/doc/dogtail/tests',
-                                        tests() ),
-                                ('share/dogtail/glade', ['sniff/sniff.ui']),
-                                ('share/dogtail/icons', sniff_icons() ),
-                                ('share/applications', ['sniff/sniff.desktop']),
-                                ('share/icons/hicolor/48x48/apps', icons('png')),
-                                ('share/icons/hicolor/scalable/apps', icons('svg'))
-                                ],
-        cmdclass = {
-                'bdist_rpm': bdist_rpm
-                }
+    author_email='dogtail-list@gnome.org',
+    url='http://dogtail.fedorahosted.org/',
+    packages=['dogtail'],
+    scripts=scripts(),
+    data_files=[
+        ('share/doc/dogtail/examples',
+        examples()),
+        ('share/doc/dogtail/examples/data',
+        examples_data()),
+        ('share/doc/dogtail/tests',
+        tests()),
+        ('share/dogtail/glade', ['sniff/sniff.ui']),
+        ('share/dogtail/icons', sniff_icons()),
+        ('share/applications', ['sniff/sniff.desktop']),
+        ('share/icons/hicolor/48x48/apps', icons('png')),
+        ('share/icons/hicolor/scalable/apps', icons('svg'))
+    ],
+    cmdclass={
+        'bdist_rpm': bdist_rpm
+    }
 )
 
 # vim: sw=4 ts=4 sts=4 noet ai
